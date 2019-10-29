d12cb608e293cea46d41915591f2bac4f56ee99b
shuaiqing <s_q@zju.edu.cn>
2019-10-29 21:32:47 +0800

    add new style dataset

--- smplifyx/data_parser.py
+++ smplifyx/data_parser.py
@@ -45,6 +45,8 @@ Keypoints.__new__.__defaults__ = (None,) * len(Keypoints._fields)
 def create_dataset(dataset='openpose', data_folder='data', **kwargs):
     if dataset.lower() == 'openpose':
         return OpenPose(data_folder, **kwargs)
+    elif dataset.lower() == 'openpose_new':
+        return OpenPoseKp(data_folder, **kwargs)
     else:
         raise ValueError('Unknown dataset: {}'.format(dataset))
 
@@ -99,7 +101,36 @@ def read_keypoints(keypoint_fn, use_hands=True, use_face=True,
     return Keypoints(keypoints=keypoints, gender_pd=gender_pd,
                      gender_gt=gender_gt)
 
+def read_keypoints_new(info, idx, use_hands=True, use_face=True,
+                   use_face_contour=False):
+    keypoints = []
 
+    gender_pd = []
+    gender_gt = []
+    while True:
+        body_keypoints = info['keypoints2d'][idx]
+        if use_hands:
+            left_hand_keyp = info['handl2d'][idx]
+            right_hand_keyp = info['handr2d'][idx]
+            body_keypoints = np.concatenate(
+                [body_keypoints, left_hand_keyp, right_hand_keyp], axis=0)
+        if use_face:
+            # TODO: Make parameters, 17 is the offset for the eye brows,
+            # etc. 51 is the total number of FLAME compatible landmarks
+            face_keypoints = info['face2d'][idx, 17: 17 + 51, :]
+
+            contour_keyps = np.array(
+                [], dtype=body_keypoints.dtype).reshape(0, 3)
+            if use_face_contour:
+                raise NotImplementedError
+            body_keypoints = np.concatenate(
+                [body_keypoints, face_keypoints, contour_keyps], axis=0)
+
+        keypoints.append(body_keypoints)
+        break
+
+    return Keypoints(keypoints=keypoints, gender_pd=gender_pd,
+                     gender_gt=gender_gt)
 class OpenPose(Dataset):
 
     NUM_BODY_JOINTS = 25
@@ -213,3 +244,110 @@ class OpenPose(Dataset):
         self.cnt += 1
 
         return self.read_item(img_path)
+
+class OpenPoseKp(Dataset):
+
+    NUM_BODY_JOINTS = 25
+    NUM_HAND_JOINTS = 20
+
+    def __init__(self, data_folder, info=None,
+                 use_hands=False,
+                 use_face=False,
+                 dtype=torch.float32,
+                 model_type='smplx',
+                 joints_to_ign=None,
+                 use_face_contour=False,
+                 openpose_format='coco25',
+                 **kwargs):
+        super(OpenPoseKp, self).__init__()
+
+        self.use_hands = use_hands
+        self.use_face = use_face
+        self.model_type = model_type
+        self.dtype = dtype
+        self.joints_to_ign = joints_to_ign
+        self.use_face_contour = use_face_contour
+
+        self.openpose_format = openpose_format
+
+        self.num_joints = (self.NUM_BODY_JOINTS +
+                           2 * self.NUM_HAND_JOINTS * use_hands)
+
+        self.img_folder = data_folder
+        self.img_paths = [osp.join(self.img_folder, img_fn)
+                          for img_fn in info['imgnames']]
+        self.info = info
+        self.cnt = 0
+
+    def get_model2data(self):
+        return smpl_to_openpose(self.model_type, use_hands=self.use_hands,
+                                use_face=self.use_face,
+                                use_face_contour=self.use_face_contour,
+                                openpose_format=self.openpose_format)
+
+    def get_left_shoulder(self):
+        return 2
+
+    def get_right_shoulder(self):
+        return 5
+
+    def get_joint_weights(self):
+        # The weights for the joint terms in the optimization
+        optim_weights = np.ones(self.num_joints + 2 * self.use_hands +
+                                self.use_face * 51 +
+                                17 * self.use_face_contour,
+                                dtype=np.float32)
+
+        # Neck, Left and right hip
+        # These joints are ignored because SMPL has no neck joint and the
+        # annotation of the hips is ambiguous.
+        if self.joints_to_ign is not None and -1 not in self.joints_to_ign:
+            optim_weights[self.joints_to_ign] = 0.
+        return torch.tensor(optim_weights, dtype=self.dtype)
+
+    def __len__(self):
+        return len(self.img_paths)
+
+    def __getitem__(self, idx):
+        img_path = self.img_paths[idx]
+        return self.read_item(img_path, idx)
+
+    def read_item(self, img_path, idx):
+        print(img_path)
+        img = cv2.imread(img_path).astype(np.float32)[:, :, ::-1] / 255.0
+        img_fn = osp.split(img_path)[1]
+        img_fn, _ = osp.splitext(osp.split(img_path)[1])
+
+        keyp_tuple = read_keypoints_new(self.info, idx, use_hands=self.use_hands,
+                                    use_face=self.use_face,
+                                    use_face_contour=self.use_face_contour)
+
+        if len(keyp_tuple.keypoints) < 1:
+            return {}
+        keypoints = np.stack(keyp_tuple.keypoints)
+
+        output_dict = {'fn': img_fn,
+                       'img_path': img_path,
+                       'keypoints': keypoints, 'img': img}
+        if keyp_tuple.gender_gt is not None:
+            if len(keyp_tuple.gender_gt) > 0:
+                output_dict['gender_gt'] = keyp_tuple.gender_gt
+        if keyp_tuple.gender_pd is not None:
+            if len(keyp_tuple.gender_pd) > 0:
+                output_dict['gender_pd'] = keyp_tuple.gender_pd
+        return output_dict
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return self.next()
+
+    def next(self):
+        if self.cnt >= len(self.img_paths):
+            raise StopIteration
+
+        img_path = self.img_paths[self.cnt]
+        self.cnt += 1
+
+        return self.read_item(img_path, self.cnt)
